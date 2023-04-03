a6ab32f315999dc8d27cf66c7ac312091c0dae76
Carlos J <carlosjorge@gmail.com>
2023-04-03 18:25:43 -0300

    commit primero

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var swiper = new Swiper(".mySwiper1", {
+    slidesPerView: 4,
+    spaceBetween: 0, 
+    slidesPerGroup: 4,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination1",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 4,
+    spaceBetween: 0, 
+    slidesPerGroup: 4,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination2",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
